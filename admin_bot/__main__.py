3ebf9683a730cf7eefdb8a0f8d2746fd135ac1d7
KenwoodFox <kenwood364@gmail.com>
2022-03-23 16:36:24 -0400

    Fixup! Make Endless

--- admin_bot/__main__.py
+++ admin_bot/__main__.py
@@ -1,1 +1,17 @@
+# Tidal Force Robotics
+# 2021, Email Blaster
+# MIT License
+
+import os
+import time
+
+
 print("Hello world!")
+
+version = str(os.environ.get("GIT_COMMIT"))
+
+print(f"Version {version}")
+
+
+while True:
+    time.sleep(10)
--- admin_bot/main.py
+++ admin_bot/main.py
@@ -1,6 +1,3 @@
 # Tidal Force Robotics
 # 2021, Email Blaster
 # MIT License
-
-
-print("Hello world!")
