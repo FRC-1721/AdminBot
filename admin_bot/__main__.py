1a147bf0348443702db1b783ea7ed59fd7f297d5
KenwoodFox <kenwood364@gmail.com>
2022-03-23 18:30:50 -0400

    Make up info cog

--- admin_bot/__main__.py
+++ admin_bot/__main__.py
@@ -1,17 +1,10 @@
-# Tidal Force Robotics
-# 2021, Email Blaster
+# Tidal Force robotics
+# 2021, Admin Bot
 # MIT License
 
-import os
-import time
 
+from .main import AdminBot
 
-print("Hello world!")
+bot = AdminBot()
 
-version = str(os.environ.get("GIT_COMMIT"))
-
-print(f"Version {version}")
-
-
-while True:
-    time.sleep(10)
+bot.run()
---
+++ admin_bot/cogs/info.py
@@ -0,0 +1,29 @@
+# Tidal Force robotics
+# 2021, Admin Bot
+# MIT License
+
+
+import discord
+
+from discord.ext import commands
+
+
+class InfoCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        status_channel = self.bot.get_channel(int("590312300695650305"))
+
+        await status_channel.send(
+            f"Admin Bot version {self.bot.version} just restarted."
+        )
+
+    @commands.command()
+    async def version(self, ctx, *, member: discord.Member = None):
+        await ctx.send(f"I am running version {self.bot.version}.")
+
+
+def setup(bot):
+    bot.add_cog(InfoCog(bot))
