43b28c1c3b11ad263a9819aae3976ce08c95721d
KenwoodFox <kenwood364@gmail.com>
2022-03-23 21:56:14 -0400

    Clean up command tree

--- admin_bot/cogs/subteams.py
+++ admin_bot/cogs/subteams.py
@@ -14,11 +14,8 @@ class SubteamCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.has_role("Team Member")
-    async def subteam(self, ctx, *args, member: discord.Member = None):
         # These should be defined maybe in a yaml somewhere else
-        self_assignable_roles = [
+        self.self_assignable_roles = [
             "Chairmans",
             "Kode Team",
             "Mechanical Team",
@@ -31,50 +28,61 @@ class SubteamCog(commands.Cog):
             "Student",
         ]
 
+    @commands.command()
+    @commands.has_role("Team Member")
+    async def addroles(self, ctx, *args, member: discord.Member = None):
         # Adds a user to a group
         roles = args[1:]
         role_objects = []
         user = ctx.message.author
 
-        if args[0].lower() == "add":
-            logging.info(f"Attempting to add user to {roles}")
+        logging.info(f"Attempting to add user to {roles}")
 
-            if all(item in self_assignable_roles for item in roles):
-                for role in roles:
-                    role_objects.append(
-                        discord.utils.get(ctx.message.guild.roles, name=role)
-                    )
-
-                await user.add_roles(*role_objects)
-                await ctx.send(f"You've been added to the following roles: {roles}")
-            else:
-                await ctx.send(
-                    f"Invalid role name or unassignable role (make sure you spelt it correctly, and used qoutes, ie: `Kode Team` or `Chairmans`) Role was: {roles}"
+        if all(item in self.self_assignable_roles for item in roles):
+            for role in roles:
+                role_objects.append(
+                    discord.utils.get(ctx.message.guild.roles, name=role)
                 )
 
-        elif args[0].lower() == "remove":
-            if all(item in self_assignable_roles for item in roles):
-                for role in roles:
-                    role_objects.append(
-                        discord.utils.get(ctx.message.guild.roles, name=role)
-                    )
-
-                await user.remove_roles(*role_objects)
-                await ctx.send(f"You've been removed from the following roles: {roles}")
-            else:
-                await ctx.send(
-                    f"Invalid role name or unassignable role (make sure you spelt it correctly, and used qoutes, ie: `Kode Team` or `Chairmans`) Role was: {roles}"
-                )
-
-        elif args[0].lower() == "list":
-            rolestr = ""
-            for role in self_assignable_roles:
-                rolestr += f"`{role}`, "
-            await ctx.send(f"The possible roles are: {rolestr}")
-
+            await user.add_roles(*role_objects)
+            await ctx.send(f"You've been added to the following roles: {roles}")
         else:
-            logging.debug(roles)
-            await ctx.send(f"Unknown argument {args[0]}")
+            await ctx.send(
+                f"Invalid role name or unassignable role (make sure you spelt it correctly, and used qoutes, ie: `Kode Team` or `Chairmans`) Role was: {roles}"
+            )
+
+    @commands.command()
+    @commands.has_role("Team Member")
+    async def removeroles(self, ctx, *args, member: discord.Member = None):
+        # Adds a user to a group
+        roles = args[1:]
+        role_objects = []
+        user = ctx.message.author
+        if all(item in self.self_assignable_roles for item in roles):
+            for role in roles:
+                role_objects.append(
+                    discord.utils.get(ctx.message.guild.roles, name=role)
+                )
+
+            await user.remove_roles(*role_objects)
+            await ctx.send(f"You've been removed from the following roles: {roles}")
+        else:
+            await ctx.send(
+                f"Invalid role name or unassignable role (make sure you spelt it correctly, and used qoutes, ie: `Kode Team` or `Chairmans`) Role was: {roles}"
+            )
+
+    @commands.command()
+    @commands.has_role("Team Member")
+    async def listroles(self, ctx, *args, member: discord.Member = None):
+        # Adds a user to a group
+        roles = args[1:]
+        role_objects = []
+        user = ctx.message.author
+
+        rolestr = ""
+        for role in self.self_assignable_roles:
+            rolestr += f"`{role}`, "
+        await ctx.send(f"The possible roles are: {rolestr}")
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
