afc4561d5778e0e9143871677e6032f491ceff59
NotSimon5673 <95383790+NotSimon5673@users.noreply.github.com>
2022-11-04 13:39:47 -0400

    I made the bee reset Improveder (#35)
    * make the thing work. HOPEFULLY Logan will no longer be able to usee the command.
    
    * I made a silly mistake by coding it so that it would ONLY let logan use the command. once this gets pushed to main pls don't delete the branch because for some reason no matter what i do it doesn't want to work :(
    
    * Update admin_bot/cogs/misc.py
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: Jack Jewett <tum0rman313@pop-os.localdomain>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- admin_bot/cogs/misc.py
+++ admin_bot/cogs/misc.py
@@ -93,15 +93,14 @@ class MiscCog(commands.Cog, name="Misc"):
 
         if await self.bot.check_spam(ctx):
             return
-
-        if ctx.author.name == "darkstar":
+        if ctx.author.name != "darkstar":
             self.bee_movie_line = 0
             await ctx.send(str("Bee movie script has been reset"))
         else:
             await ctx.message.add_reaction("😂")
             await ctx.send(
                 str(
-                    "you, darkstar, can no longer use this command. Better luck next time!"
+                    "you, Logan(AKA darkstar), can no longer use this command. Better luck next time!"
                 )
             )
 
