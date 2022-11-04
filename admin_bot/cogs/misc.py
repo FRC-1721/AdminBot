6f8321b085fe90b3bd579815393845897b81eab7
NotSimon5673 <95383790+NotSimon5673@users.noreply.github.com>
2022-11-04 13:33:06 -0400

    Update admin_bot/cogs/misc.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- admin_bot/cogs/misc.py
+++ admin_bot/cogs/misc.py
@@ -93,7 +93,6 @@ class MiscCog(commands.Cog, name="Misc"):
 
         if await self.bot.check_spam(ctx):
             return
-            
         if ctx.author.name != "darkstar":
             self.bee_movie_line = 0
             await ctx.send(str("Bee movie script has been reset"))
