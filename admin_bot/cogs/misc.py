0efdff6873dfac1913d3c03a43c835f1fc153993
KenwoodFox <kenwood364@gmail.com>
2023-01-17 15:34:25 -0500

    use mjpeg instead

--- admin_bot/cogs/misc.py
+++ admin_bot/cogs/misc.py
@@ -60,7 +60,7 @@ class MiscCog(commands.Cog, name="Misc"):
         HIGH_VALUE = 10000
         WIDTH = HIGH_VALUE
         HEIGHT = HIGH_VALUE
-        fourcc = cv.VideoWriter_fourcc(*"XVID")
+        fourcc = cv.VideoWriter_fourcc(*"MJPG")
         cap.set(cv.CAP_PROP_FRAME_WIDTH, WIDTH)
         cap.set(cv.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 
